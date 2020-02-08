44cd6edc16de50c9eca37ffe3dfd3b15eb8e148e
Mikkel Gade <mg@cego.dk>
2020-02-08 14:20:38 +0100

    Use Gson to serialize response

--- src/main/java/rest/AuthorResource.java
+++ src/main/java/rest/AuthorResource.java
@@ -1,19 +1,33 @@
 package rest;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import com.google.gson.Gson;
+
+import author.Author;
+import logging.Logger.Severity;
+import logging.SysoutLogger;
+import parsing.AuthorParser;
+import parsing.Parser;
 
 /**
  * Resource for handling author name requests
  */
 @Path("author")
 public class AuthorResource {
+    
+    private Gson gson = new Gson();
 
-    @GET
+    @POST
     @Produces(MediaType.APPLICATION_JSON)
-    public String getIt() {
-        return "{\"message\":\"Got it! 2\"}";
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response getMsg(String data) {
+        
+        Parser<Author> parser = new AuthorParser(new SysoutLogger(Severity.DEBUG));
+        Author a = parser.parse(data);
+        
+        return Response.status(200).entity(gson.toJson(a)).build();
     }
 }
